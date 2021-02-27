9bf779a1e82655ad56654cf3999a5e0a3a9f7981
fatheaddrummer <saite@gmx.at>
2021-02-27 11:45:27 +0100

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,63 +1,63 @@
-<template>
-  <div class="form">
-    <slot :submit="submit" :spinner="spinner"></slot>
-  </div>
-</template>
+import Vue from 'vue'
+import { VueReCaptcha } from 'vue-recaptcha-v3'
 
-<script>
+class FormService{
 
-  import Vue from 'vue'
-  import { VueReCaptcha } from 'vue-recaptcha-v3'
+  site_key = String
+  form_id = String
+  mounted = false
+  spinner = false
 
-  export default {
-    props:['form_id', 'recaptcha_site_key'],
-    data:function(){
-      return {
-        show:true,
-        spinner:false,
-      }
-    },
-    mounted:function(){
-      Vue.use(VueReCaptcha, { siteKey: this.recaptcha_site_key })
-    },
-    methods: {
-      async submit({event, form}) {
+  constructor(context) {
+    this.context = context
+  }
 
-        //start spinner
-        this.spinner = true
-
-        //prevent http form submission
-        event.preventDefault()
-
-        // build native form
-        var bodyFormData = new FormData();
-        var form_keys = Object.keys(form)
-        form_keys.forEach(key=>{
-          bodyFormData.append(key, form[key]);
-        })
-
-        //load and add recaptcha
-        await this.$recaptchaLoaded()
-        var token = await this.$recaptcha('login')
-        bodyFormData.append('_wpcf7_recaptcha_response', token);
-
-        //prepare multipart axios form body
-        const options = {
-          method: 'POST',
-          headers: { 'content-type': 'multipart/form-data' },
-          data: bodyFormData,
-          url:`/contact-form-7/v1/contact-forms/${this.form_id}/feedback`,
-        };
-
-        //send form submission
-        var response = await this.$axios(options);
-        
-        //send event to parent component
-        this.$emit('submit', response)
-
-        //stop spinner
-        this.spinner = false
-      },
+  mount(){
+    if(!this.mounted){
+      Vue.use(VueReCaptcha, { siteKey: this.site_key })
+      this.mounted = true
     }
   }
-</script>
+
+  set_site_key(site_key){
+    this.site_key = site_key
+  }
+
+  set_form_id(form_id){
+    this.form_id = form_id
+  }
+
+  async submit(form){
+    //start spinner
+    this.spinner = true
+    
+    // build native form
+    var bodyFormData = new FormData();
+    var form_keys = Object.keys(form)
+    form_keys.forEach(key=>{
+      bodyFormData.append(key, form[key]);
+    })
+
+    //load and add recaptcha
+    var vm = new Vue({})
+    await vm.$recaptchaLoaded()
+    var token = await vm.$root.$recaptcha('login')
+    bodyFormData.append('_wpcf7_recaptcha_response', token);
+
+    //prepare multipart axios form body
+    const options = {
+      method: 'POST',
+      headers: { 'content-type': 'multipart/form-data' },
+      data: bodyFormData,
+      url:`/contact-form-7/v1/contact-forms/${this.form_id}/feedback`,
+    };
+
+    //send form submission
+    return await this.context.$axios(options);
+  }
+
+}
+
+export default (context, inject) => {
+  inject('formService', new FormService(context))
+}
